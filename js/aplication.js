3643f18ea9cd168cea08f166f8069551553ec660
Pancho Miqueles <panchomr.2000@uc.cl>
2021-10-12 00:54:20 -0300

    3 mapas

--- js/aplication.js
+++ js/aplication.js
@@ -13,6 +13,28 @@ $(document).ready(function(){
             $('#'+idpais+'1').fadeTo("fast", 0.5)       
             $("#prueba").text(idpais)
         }
+        if (idpais.includes('2')){
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 0.5)
+            $('#'+newidpais+'1').fadeTo("fast", 0.5)
+        }
+        else {
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 0.5)
+            $('#'+newidpais+'1').fadeTo("fast", 0.5)       
+            $("#prueba").text(idpais)
+        }
+        if (idpais.includes('1')){
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 0.5)
+            $('#'+newidpais+'2').fadeTo("fast", 0.5)
+        }
+        else {
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 0.5)
+            $('#'+newidpais+'2').fadeTo("fast", 0.5)       
+            $("#prueba").text(idpais)
+        }
     });
 
     paises.mouseleave(function(){
@@ -27,24 +49,42 @@ $(document).ready(function(){
             $('#'+idpais+'1').fadeTo("fast", 1)       
             $("#prueba").text(idpais)
         }
+        if (idpais.includes('2')){
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 1)
+            $('#'+newidpais+'1').fadeTo("fast", 1)
+        }
+        else {
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 1)
+            $('#'+newidpais+'1').fadeTo("fast", 1)       
+            $("#prueba").text(idpais)
+        }
+        if (idpais.includes('1')){
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 1)
+            $('#'+newidpais+'2').fadeTo("fast", 1)
+        }
+        else {
+            var newidpais = idpais.substring(0, idpais.length - 1)
+            $('#'+idpais).fadeTo("fast", 1)
+            $('#'+newidpais+'2').fadeTo("fast", 1)       
+            $("#prueba").text(idpais)
+        }
     });
 
 });
 
 $(document).ready(function(){
-    var boton1 = $(".boton1");
-    var idboton1 = 0;
+    var opcion1 = $("#opcion1");
 
-
-    boton1.click(function(){
-        if (idboton1 == 0){
-            $("#titulo").fadeTo("fast", 0);
-            idboton1 = 1
-        }
-        else if (idboton1 == 1){
-            $("#titulo").fadeTo("fast", 1);
-            idboton1 = 0
-        }
+    opcion1.click(function(){
+        $(".titulo").hide();
+        $(".descripcion").hide();
+        $("#mapa_izq").hide();
+        $("#mapa_nuevo").show();
+        $("#h3").show();
+        $("#p3").show();
     });
 
 });
