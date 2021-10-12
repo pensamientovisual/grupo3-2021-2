dd319275676e3d1df7bd553dd4ab80e29a51038f
Rosario <ralvar.1928@gmail.com>
2021-10-12 13:09:58 -0300

    intento leyenda whr

--- js/aplication.js
+++ js/aplication.js
@@ -99,5 +99,46 @@ $(document).ready(function(){
             idboton = 0
         }
     });
+});
 
+// De acá en adelante es el intento de leyenda WHR
+var margin = {top: 30, right: 30, bottom: 100, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+$(document).ready(function(){
+    const svg = d3.select("#legend1")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+  
+  
+      
+  
+      svg.append("circle").attr("cx",200).attr("cy",10).attr("r", 6).style("fill", "#dbfff6")
+      svg.append("circle").attr("cx",200).attr("cy",70).attr("r", 6).style("fill", "#81a69b")
+      svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#5d8276")
+      svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#51766a")
+      svg.append("circle").attr("cx",200).attr("cy",190).attr("r", 6).style("fill", "#466b5f")
+      svg.append("circle").attr("cx",200).attr("cy",220).attr("r", 6).style("fill", "#3a6054")
+      svg.append("circle").attr("cx",200).attr("cy",250).attr("r", 6).style("fill", "#2f5548")
+      svg.append("circle").attr("cx",200).attr("cy",280).attr("r", 6).style("fill", "#244a3e")
+      svg.append("circle").attr("cx",200).attr("cy",310).attr("r", 6).style("fill", "#193f33")
+      svg.append("circle").attr("cx",200).attr("cy",340).attr("r", 6).style("fill", "#0d3529")
+      svg.append("circle").attr("cx",200).attr("cy",370).attr("r", 6).style("fill", "#022b1f")
+      
+      svg.append("text").attr("x", 220).attr("y", 10).text("[36 , 38)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 40).text("...").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 70).text("[50 , 52)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 100).text("...").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 130).text("[56 , 58)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 160).text("[58 , 60)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 190).text("[60 , 62)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 220).text("[62 , 64)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 250).text("[64 , 66)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 280).text("[66 , 68)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 310).text("[68 , 70)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 340).text("[70 , 72)").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg.append("text").attr("x", 220).attr("y", 370).text("[72 , 74)").style("font-size", "15px").attr("alignment-baseline","middle")
+      
 });
