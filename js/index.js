7ccc89685f3d43089ae6f6b037d5c0bb5090335e
Rosario <ralvar.1928@gmail.com>
2021-10-25 21:27:16 -0300

    Versión preliminar página inicial

---
+++ js/index.js
@@ -0,0 +1,17 @@
+d3.xml("mapa-svg/america-bordes.svg")
+  .then(data => {
+    d3.select("#svg-mapa").node().append(data.documentElement)
+  });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
