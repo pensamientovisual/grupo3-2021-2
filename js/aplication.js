4bc9dad7d31f74278cc0ce035916811aa6a43b15
Pancho Miqueles <panchomr.2000@uc.cl>
2021-10-26 22:55:19 -0300

    avances

--- js/aplication.js
+++ js/aplication.js
@@ -1,76 +1,19 @@
 $(document).ready(function(){
     var paises = $(".pais");
+    var datos_happy = [['Argentina', 59.7], ['Bolivia', 57.5], ['Brazil', 63.8], ['Canada', 72.3], ['Chile', 62.3], ['Cameroon', 50.8], ['"Congo', 0.0], ['"Congo', 54.9], ['Colombia', 61.6], ['Costa Rica', 71.2], ['Cuba', 0.0], ['Dominican Republic', 56.9], ['Ecuador', 59.3], ['Guatemala', 64.0], ['Guyana', 0.0], ['Honduras', 59.5], ['Haiti', 37.2], ['Jamaica', 58.9] ['Maldives', 52.0], ['Mexico', 64.7], ['Montenegro', 55.5], ['Nicaragua', 61.4], ['Panama', 63.0], ['Peru', 58.0], ['Paraguay', 56.9], ['Puerto Rico', 0] ['El Salvador', 63.5], ['Uruguay', 64.4], ['United States', 69.4], ['Venezuela', 50.5]]
+    var datos_income = [['Argentina', 22100], ['Bahamas', 37100], ['Bosnia and Herzegovina', 14900], ['Bolivia', 8720], ['Brazil', 14700], ['Canada', 48900], ['Switzerland', 68400], ['Chile', 25100], ['Cameroon', 3640], ['"Congo', 1090], ['"Congo', 3910], ['Colombia', 14600], ['Costa Rica', 20300], ['Cuba', 9790], ['Dominican Republic', 18400], ['Ecuador', 11400], ['Guatemala', 8640], ['Honduras', 5730], ['Haiti', 2910], ['Jamaica', 9760], ['St. Kitts and Nevis', 26400], ['St. Lucia', 15400], ['Maldives', 19500], ['Mexico', 19800], ['Montenegro', 21600], ['Nicaragua', 5410], ['Panama', 31500], ['Peru', 12800], ['Paraguay', 12700], ['El Salvador', 8780], ['Trinidad and Tobago', 26200], ['Uruguay', 21600], ['United States', 62500], ['St. Vincent and the Grenadines', 12500], ['Venezuela', 8320]]
+
 
     paises.mouseenter(function(){
         var idpais = $(this).attr("id");
-        if (idpais.includes('1')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+newidpais).fadeTo("fast", 0.5)
-        }
-        else {
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+idpais+'1').fadeTo("fast", 0.5)       
-            $("#prueba").text(idpais)
-        }
-        if (idpais.includes('2')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+newidpais+'1').fadeTo("fast", 0.5)
-        }
-        else {
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+newidpais+'1').fadeTo("fast", 0.5)       
-            $("#prueba").text(idpais)
-        }
-        if (idpais.includes('1')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+newidpais+'2').fadeTo("fast", 0.5)
-        }
-        else {
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 0.5)
-            $('#'+newidpais+'2').fadeTo("fast", 0.5)       
-            $("#prueba").text(idpais)
-        }
+        $('#'+idpais).fadeTo("fast", 0.5)
+        
     });
 
     paises.mouseleave(function(){
-        var idpais = $(this).attr("id");
-        if (idpais.includes('1')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+newidpais).fadeTo("fast", 1)
-        }
-        else {
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+idpais+'1').fadeTo("fast", 1)       
-            $("#prueba").text(idpais)
-        }
-        if (idpais.includes('2')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+newidpais+'1').fadeTo("fast", 1)
-        }
-        else {
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+newidpais+'1').fadeTo("fast", 1)       
-            $("#prueba").text(idpais)
-        }
-        if (idpais.includes('1')){
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+newidpais+'2').fadeTo("fast", 1)
-        }
-        else {
-            var newidpais = idpais.substring(0, idpais.length - 1)
-            $('#'+idpais).fadeTo("fast", 1)
-            $('#'+newidpais+'2').fadeTo("fast", 1)       
-            $("#prueba").text(idpais)
-        }
+        var idpais = $(this).attr("id"); 
+        $('#'+idpais).fadeTo("fast", 1)
+        
     });
 
 });
