8e17c6ba25975971cbaa076591358aef13dad75e
Pancho Miqueles <panchomr.2000@uc.cl>
2021-10-28 04:21:09 -0300

    3 graficos

--- js/barras_income.js
+++ js/barras_income.js
@@ -1,33 +1,31 @@
 // https://bl.ocks.org/anonymous/bc5a9691a3417b403d4e8ade3297afa3/3a2434c1c2849e476791e581754ec27e055db4d6
 
-var data = [
-    {key: 'Argentina', value: 59.7, income: 22100, dh: 0.83},
-    {key: 'Bolivia', value: 57.5, income: 8720, dh: 0.703},
-    {key: 'Brazil', value: 63.8, income: 14700, dh: 0.761},
-    {key: 'Canada', value: 72.3, income: 48900, dh: 0.922},
-    {key: 'Chile', value: 62.3, income: 25100, dh: 0.847},
-    {key: 'Colombia', value: 61.6, income: 14600, dh: 0.761},
-    {key: 'Costa Rica', value: 71.2, income: 20300, dh: 0.794},
-    {key: 'Ecuador', value: 59.3, income: 11400, dh: 0.758},
-    {key: 'El Salvador', value: 63.5, income: 8780, dh: 0.667},
-    {key: 'United States', value: 69.4, income: 62500, dh: 0.92},
-    {key: 'Guatemala', value: 64.0, income: 8640, dh: 0.651},
-    {key: 'Haiti', value: 37.2, income: 2910, dh: 0.503},
-    {key: 'Honduras', value: 59.5, income: 5730, dh: 0.623},
-    {key: 'Jamaica', value: 58.9, income: 9760, dh: 0.726},
-    {key: 'Mexico', value: 64.7, income: 19800, dh: 0.767},
-    {key: 'Nicaragua', value: 61.4, income: 5410, dh: 0.651},
-    {key: 'Panama', value: 63.0, income: 31500, dh: 0.795},
-    {key: 'Paraguay', value: 56.9, income: 12700, dh: 0.724},
-    {key: 'Peru', value: 58.0, income: 12800, dh: 0.759},
-    {key: 'Dominican Republic', value: 56.9, income: 18400, dh: 0.745},
-    {key: 'Uruguay', value: 64.4, income: 21600, dh: 0.808},
-    {key: 'Venezuela', value: 50.5, income: 8320, dh: 0.726}
-];
-
-
-    
-var w = 700;
+var data1 = [
+    {key: 'Argentina', value: 22100, income: 59.7, dh: 0.83},
+    {key: 'Bolivia', value: 8720, income: 57.5, dh: 0.703},
+    {key: 'Brazil', value: 14700, income: 63.8, dh: 0.761},
+    {key: 'Canada', value: 48900, income: 72.3, dh: 0.922},
+    {key: 'Chile', value: 25100, income: 62.3, dh: 0.847},
+    {key: 'Colombia', value: 14600, income: 61.6, dh: 0.761},
+    {key: 'Costa Rica', value: 20300, income: 71.2, dh: 0.794},
+    {key: 'Ecuador', value: 11400, income: 59.3, dh: 0.758},
+    {key: 'El Salvador', value: 8780, income: 63.5, dh: 0.667},
+    {key: 'United States', value: 62500, income: 69.4, dh: 0.92},
+    {key: 'Guatemala', value: 8640, income: 64.0, dh: 0.651},
+    {key: 'Haiti', value: 2910, income: 37.2, dh: 0.503},
+    {key: 'Honduras', value: 5730, income: 59.5, dh: 0.623},
+    {key: 'Jamaica', value: 9760, income: 58.9, dh: 0.726},
+    {key: 'Mexico', value: 19800, income: 64.7, dh: 0.767},
+    {key: 'Nicaragua', value: 5410, income: 61.4, dh: 0.651},
+    {key: 'Panama', value: 31500, income: 63.0, dh: 0.795},
+    {key: 'Paraguay', value: 12700, income: 56.9, dh: 0.724},
+    {key: 'Peru', value: 12800, income: 58.0, dh: 0.759},
+    {key: 'Dominican Republic', value: 18400, income: 56.9, dh: 0.745},
+    {key: 'Uruguay', value: 21600, income: 64.4, dh: 0.808},
+    {key: 'Venezuela', value: 8320, income: 50.5, dh: 0.726}
+  ];
+      
+  var w = 700;
 var h = 500;
 var margin = {
     top: 50,
@@ -39,14 +37,14 @@ var width = w - margin.left - margin.right;
 var height = h - margin.top - margin.bottom;
     
 var x = d3.scaleBand()
-        .domain(data.map(function(d){
+        .domain(data1.map(function(d){
             return d.key;
         }))
         .range([margin.left, width])
     		.padding(0.1);
     
 var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d){
+        .domain([0, d3.max(data1, function(d){
             return d.value;
         })])
 				.range([height, margin.top])
@@ -56,40 +54,34 @@ var yAxis = d3.axisLeft().scale(y)
 var paises = $(".pais")
 
 var svg = d3.select("body").append("svg")
-            .attr("id", "chart")
+            .attr("id", "chart1")
             .attr("width", w)
             .attr("height", h);
     				
-						svg.append("g")
-						.attr("class","axis")
-						.attr("transform","translate(" + margin.left + ",0)")
-						.call(yAxis);
+            svg.append("g")
+            .attr("class","axis")
+            .attr("transform","translate(" + margin.left + ",0)")
+            .call(yAxis);
          
 
     svg.selectAll("rect")
-        .data(data)
+        .data(data1)
         .enter()
             .append("rect")
    					.attr("class","bar")
        			.on("mouseover",function(){
-              var idpais = $(this).attr("id")
-              if (this.key == idpais){
-                $('#'+idpais).fadeTo("fast", 0.5)
                 d3.select(this)
                   .attr("fill","red")
-                }
     				}) 				
     				.on("mouseout",function(){
-              var idpais = $(this).attr("id")
-              $('#'+idpais).fadeTo("fast", 1)
-              if (this.key == idpais){
+              
                 d3.select(this)
               	.transition("colorfade")
 								.duration(250)
     						.attr("fill",function(d){
       						return "rgb(" + Math.round(d.value*2) + "," 
                 + Math.round(d.value*2) + "," + Math.round(d.value*2) + ")";})
-    				    }
+    				    
             })
     
     				.attr("fill",function(d){
@@ -125,7 +117,7 @@ var svg = d3.select("body").append("svg")
 
 
     svg.selectAll(".val-label")
-        .data(data)
+        .data(data1)
         .enter()
             .append("text")
             .classed("val-label", true)
@@ -148,7 +140,7 @@ var svg = d3.select("body").append("svg")
     				.text(function(d){return d.value});
   
   	svg.selectAll(".bar-label")
-            .data(data)
+            .data(data1)
         		.enter()
             .append("text")
             .classed("bar-label", true)
@@ -160,11 +152,11 @@ var svg = d3.select("body").append("svg")
       			.attr("text-anchor","left")
     				.text(function(d){return d.key})
     
-    d3.select("#byKey").on("click", function() {
-  data.sort(function(a, b,c) {
+    d3.select("#byKey1").on("click", function() {
+  data1.sort(function(a, b,c) {
     return d3.ascending(a.key, b.key)
   })
-  x.domain(data.map(function(d) {
+  x.domain(data1.map(function(d) {
     return d.key;
   }));
   svg.selectAll(".bar")
@@ -190,11 +182,11 @@ var svg = d3.select("body").append("svg")
 
 })
 
-d3.select("#byValue").on("click", function() {
-  data.sort(function(a, b,c) {
+d3.select("#byValue1").on("click", function() {
+  data1.sort(function(a, b,c) {
     return d3.descending(a.value, b.value)
   })
-  x.domain(data.map(function(d) {
+  x.domain(data1.map(function(d) {
     return d.key;
   }));
   svg.selectAll(".bar")
@@ -220,11 +212,11 @@ d3.select("#byValue").on("click", function() {
 })
 
 
-d3.select("#byincome").on("click", function() {
-  data.sort(function(a, b,c) {
+d3.select("#byincome1").on("click", function() {
+  data1.sort(function(a, b,c) {
     return d3.descending(a.income, b.income)
   })
-  x.domain(data.map(function(d) {
+  x.domain(data1.map(function(d) {
     return d.key;
   }));
   svg.selectAll(".bar")
@@ -250,11 +242,11 @@ d3.select("#byincome").on("click", function() {
 })
 
 
-d3.select("#bydh").on("click", function() {
-  data.sort(function(a, b,c,d) {
+d3.select("#bydh1").on("click", function() {
+  data1.sort(function(a, b,c,d) {
     return d3.descending(a.dh, b.dh)
   })
-  x.domain(data.map(function(d) {
+  x.domain(data1.map(function(d) {
     return d.key;
   }));
   svg.selectAll(".bar")
